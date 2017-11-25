a2415643726a7cd6cb4853c0f97a68d16226fdab
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2017-11-25 22:12:22 +0300

    add creation Bullet

--- class.player.js
+++ class.player.js
@@ -11,11 +11,13 @@ class Player extends BaseEntity {
   update() {
     if (this.energy <= 0)
       return;
-    this.vx  = gameState.keysDown.ArrowRight || gameState.keysDown.Right || gameState.keysDown.d || 0;
-    this.vx -= gameState.keysDown.ArrowLeft || gameState.keysDown.Left || gameState.keysDown.a || 0;
-    this.vy  = gameState.keysDown.ArrowDown || gameState.keysDown.Down || gameState.keysDown.s || 0;
-    this.vy -= gameState.keysDown.ArrowUp || gameState.keysDown.Up || gameState.keysDown.w || 0;
+    //  TODO: 1. Set this.vx and this.vy using gameState.keysDown to move the player with the Arrow keys, or any other keys you prefer
+    this.vx  = gameState.keysDown.ArrowRight || gameState.keysDown.Right || 0;
+    this.vx -= gameState.keysDown.ArrowLeft || gameState.keysDown.Left || 0;
+    this.vy  = gameState.keysDown.ArrowDown || gameState.keysDown.Down || 0;
+    this.vy -= gameState.keysDown.ArrowUp || gameState.keysDown.Up || 0;
 
+    //TODO: 2. Set this.x and this.y to keep the player in the arena, or wrap around it.
     if (this.x < 0) this.x = 0;
     if (this.y < 0) this.y = 0;
     if (this.x > gameState.size) {
@@ -24,30 +26,20 @@ class Player extends BaseEntity {
     if (this.y > gameState.size) {
       this.y = gameState.size;
     }
-    // const max = gameState.size;
-    // if (this.x < 0) this.x = 0;
-    // if (this.y < 0) this.y = 0;
-    // if (this.x > max) this.x = max;
-    // if (this.y > max) this.y = max;
 
+    //TODO: 3. Check gameState.keysDown to spawn a new Bullet and set its this.vx and this.vy
+    let bulletVx,bulletVy;
+    bulletVx = gameState.keysDown.d || 0;
+    bulletVx -= gameState.keysDown.a || 0;
+    bulletVy = gameState.keysDown.s || 0;
+    bulletVy -= gameState.keysDown.w || 0;
 
-
-
-    /*
-      SESSION #1 in the Player update method
-
-      TODO: 1. Set this.vx and this.vy using gameState.keysDown to move the player with the Arrow keys, or any other keys you prefer
-      HINT: gameState.keysDown.j is undefined if 'j' is not pressed, and 1 if it is pressed.
-      SOLUTION: solution1.js
-
-      TODO: 2. Set this.x and this.y to keep the player in the arena, or wrap around it.
-      HINT: The size of the arena is gameState.size
-      SOLUTION: solution2.js
-
-      TODO: 3. Check gameState.keysDown to spawn a new Bullet and set its this.vx and this.vy
-      HINT: gameState.keysDown.s is undefined if 's' is not pressed, and 1 if it is pressed.
-      SOLUTION: solution3.js
-    */
+    if (bulletVx || bulletVy) {
+      const speed = 4;
+      gameState.entities.push(
+        new Bullet(this.x, this.y, bulletVx * speed, bulletVy * speed)
+      )
+    };
 
     // move & render
     super.update();
